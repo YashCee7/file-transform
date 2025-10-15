326d5511e8af7178bd09657fba6802f0ffaa9a88
YashCee7 <yashchugh112@gmail.com>
2025-10-15 11:06:43 +0000

    col existence check

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ if file is not None:
     df = pd.read_csv(file)
 
     # Severity
-    if df['Risk Score']:
+    if 'Risk Score' in df.columns:
         severity_flag = True
         df['Severity'] = ''
         df['Severity'] = df['Risk Score'].apply(lambda x: 'High' if x >= 67 else 'Medium')
@@ -25,7 +25,7 @@ if file is not None:
         df.insert(1, 'Severity', severity)
 
     # Region
-    if df['Asset Hostname']:
+    if 'Asset Hostname' in df.columns:
         region_flag = True
         def regionFinder(s):
             try:
@@ -44,7 +44,7 @@ if file is not None:
         df.insert(10, 'Region', region)
 
     # Aging
-    if df['Last Seen'] and df['Found']:
+    if 'Last Seen' in df.columns and 'Found' in df.columns:
         aging_flag = True
         def dateDifference(date1, date2):
             if date1 == '' or date2 == '':
@@ -65,15 +65,15 @@ if file is not None:
         aging = df.pop('Aging')
         df.insert(7, 'Aging', aging)
 
-        if any([severity_flag, region_flag, aging_flag]):
-            buffer = BytesIO()
-            with pd.ExcelWriter(buffer, engine = "xlsxwriter") as writer:
-                df.to_excel(writer, index=False)
-            transformed_file = buffer.getvalue()
-            if st.download_button(label = "Download your transformed file here", data = transformed_file, 
+    if any([severity_flag, region_flag, aging_flag]):
+        buffer = BytesIO()
+        with pd.ExcelWriter(buffer, engine = "xlsxwriter") as writer:
+            df.to_excel(writer, index=False)
+        transformed_file = buffer.getvalue()
+        if st.download_button(label = "Download your transformed file here", data = transformed_file, 
                             mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"):
-                st.success("thank you for using this app")
-                st.stop()
-        else:
-            st.text("the file provided does not have any columns which need to be processed.")
+            st.success("thank you for using this app")
             st.stop()
+    else:
+        st.text("the file provided does not have any columns which need to be processed.")
+        st.stop()
